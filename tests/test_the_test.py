2c7cfeb1ba00ddd6c16ea5a0b0168be102189e7d
addman <addman@debian-BULLSEYE-live-builder-AMD64>
2022-06-21 08:07:12 +0200

    Add the one test

---
+++ tests/test_the_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test the functionality
+"""
+
+def test_the_test(aiida_local_code_factory, clear_database):
+    from aiida.plugins import CalculationFactory
+    from aiida.engine import run
+    from aiida.orm import Int
+
+    executable = 'python3'
+    entry_point = 'test.calc'
+
+    code = aiida_local_code_factory(entry_point=entry_point, executable=executable)
+    calculation = CalculationFactory(entry_point)
+
+    inputs = { 'code': code,
+               'koza': Int(1)}
+
+    result = run(calculation, **inputs)
+
+    assert result['ovca'] == 2

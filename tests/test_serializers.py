d2c3dd0062208705854231d383278793e895d4c9
Otto Kohulak <pravod@gmail.com>
2022-08-17 15:13:35 +0200

    Add deserializers tests

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -59,3 +59,62 @@ def test_SerializerStr():
     ans = SerializerStr(obj)
 
     assert ans is obj
+
+def test_de_SerializerInt():
+
+    from aiida.orm import Int
+    from aiida.orm import Float
+
+    from aiida_python import SerializerInt
+
+    obj = int(42)
+    ans = SerializerInt.i()(obj)
+
+    assert isinstance(ans, Int)
+    assert ans == 42
+
+    obj = float(42)
+    ans = SerializerInt.i()(obj)
+
+    assert isinstance(ans, float)
+    assert ans == 42
+
+def test_de_SerializerFloat():
+
+    from aiida.orm import Int
+    from aiida.orm import Float
+
+    from aiida_python import SerializerFloat
+
+    obj = float(2)
+    ans = SerializerFloat.i()(obj)
+
+    assert isinstance(ans, Float)
+    assert ans == 2
+
+    obj = int(2)
+    ans = SerializerFloat.i()(obj)
+
+    assert isinstance(ans, int)
+    assert ans == 2
+
+def test_de_SerializerStr():
+
+    from aiida.orm import Int
+    from aiida.orm import Str
+
+    from aiida_python import SerializerStr
+
+    message = "Anitta hassuwas"
+
+    obj = str(message)
+    ans = SerializerStr.i()(obj)
+
+    assert isinstance(ans, Str)
+    assert ans == message
+
+    obj = int(2)
+    ans = SerializerStr.i()(obj)
+
+    assert isinstance(ans, int)
+    assert ans == 2

3df0735d72ef96090926c32cb8cf3e0e33a3f239
Otto Kohulak <pravod@gmail.com>
2022-11-10 09:08:28 +0100

    WIP

--- aiida_python/serializers.py
+++ aiida_python/serializers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from aiida.orm import Int
+from aiida.orm import (Int, Float)
 
 class Serializer():
 
@@ -15,6 +15,28 @@ class SerializerInt(Serializer):
     @classmethod
     def philemon(cls):
         return Int
+
+    @classmethod
+    def serialize(cls, obj):
+        if isinstance(obj, cls.philemon()):
+            return obj.value
+        return obj
+
+    @classmethod
+    def deserialize(cls, obj):
+        if isinstance(obj, cls.baukis()):
+            return cls.philemon()(obj)
+        return obj
+
+class SerializerFloat(Serializer):
+
+    @classmethod
+    def baukis(cls):
+        return float
+
+    @classmethod
+    def philemon(cls):
+        return Float
 
     @classmethod
     def serialize(cls, obj):
--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -25,3 +25,25 @@ def test_SerializerInt():
     ans = SerializerInt.deserialize(2)
 
     assert isinstance(ans, Int)
+
+def test_SerializerInt():
+
+    from aiida.orm import Int
+    from aiida.orm import Float
+
+    from aiida_python import SerializerFloat
+
+    obj = Float(2.0)
+    ans = SerializerFloat.serialize(obj)
+
+    assert isinstance(ans, float)
+    assert ans == 2
+
+    obj = Int(2)
+    ans = SerializerInt.serialize(obj)
+
+    assert ans is obj
+
+    ans = SerializerInt.deserialize(2.0)
+
+    assert isinstance(ans, Float)

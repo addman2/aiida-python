af173a06575686842d43af64d9bbf8354cfa4909
Otto Kohulak <pravod@gmail.com>
2022-11-10 09:52:45 +0100

    WIP

--- tests/test_simple_code.py
+++ tests/test_simple_code.py
@@ -1,4 +1,4 @@
-from aiida.orm import (Int, Float)
+from aiida.orm import (Int, Float, Str)
 from aiida.plugins import CalculationFactory
 
 CalcJobPython = CalculationFactory("aiida_python.calc")
@@ -30,10 +30,26 @@ class ClassThatCannotStartWithTestFloat(CalcJobPython):
         b = a + 1
         self.outputs.ovca = b
 
+class ClassThatCannotStartWithTestStr(CalcJobPython):
+
+    @classmethod
+    def define(cls, spec):
+        super().define(spec)
+
+        spec.input('koza', valid_type=Str)
+        spec.output('ovca', valid_type=Str)
+
+    def run_python(self):
+        a = self.inputs.koza
+        if a == "sina toki uta e toki pona anu seme?":
+            b = "mi toki e toki pona lili"
+        else:
+            b = "mi sona ala"
+        self.outputs.ovca = b
+
 def test_simple_code_int(aiida_local_code_factory, clear_database):
     from aiida.plugins import CalculationFactory
     from aiida.engine import run
-    from aiida.orm import Int
 
     executable = 'python3'
     entry_point = 'test.calc_int'
@@ -51,7 +67,6 @@ def test_simple_code_int(aiida_local_code_factory, clear_database):
 def test_simple_code_float(aiida_local_code_factory, clear_database):
     from aiida.plugins import CalculationFactory
     from aiida.engine import run
-    from aiida.orm import Int
 
     executable = 'python3'
     entry_point = 'test.calc_float'
@@ -65,3 +80,27 @@ def test_simple_code_float(aiida_local_code_factory, clear_database):
     result = run(calculation, **inputs)
 
     assert result['ovca'] == 2.0
+
+def test_simple_code_str(aiida_local_code_factory, clear_database):
+    from aiida.plugins import CalculationFactory
+    from aiida.engine import run
+
+    executable = 'python3'
+    entry_point = 'test.calc_str'
+
+    code = aiida_local_code_factory(entry_point=entry_point, executable=executable)
+    calculation = CalculationFactory(entry_point)
+
+    inputs = { 'code': code,
+               'koza': Str("sina toki uta e toki pona anu seme?")}
+
+    result = run(calculation, **inputs)
+
+    assert result['ovca'] == "mi toki e toki pona lili"
+
+    inputs = { 'code': code,
+               'koza': Str("Excuse me do you speak my language?")}
+
+    result = run(calculation, **inputs)
+
+    assert result['ovca'] == "mi sona ala"

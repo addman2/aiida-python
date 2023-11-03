2fc9320d695f7a56585e2719a88a81020f4d6989
Otto Kohulak <pravod@gmail.com>
2023-11-03 18:22:03 +0100

    Add Manifest.in

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
         packages=find_packages(include=['aiida_python', 'aiida_python.*']),
         # this doesn't work when placed in setup.json (something to do with str type)
         package_data={
-            '': ['*', 'setup.json'],
+            '': [
+                '*',
+            ],
         },
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',
